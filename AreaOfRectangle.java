33427586d0d3a6dfcea92fa785e3d2b4a70f398c
sivaadithya25 <sivaadinarayanan25@email.com>
2026-03-21 09:53:04 +0530

    Add Java programs: AreaOfRectangle, PermeterOfRectangle, and Subwaygame

---
+++ AreaOfRectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = sc.nextDouble();
+        
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+        
+        sc.close();
+    }
+}
