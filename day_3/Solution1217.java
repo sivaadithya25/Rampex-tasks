8703c941c69321de5e4482bd14cdd506901d7d18
Siva Adithya Narayana A <sivaadinarayanan25@gmail.com>
2026-03-24 10:25:23 +0530

    leetcode_problem
    Implement method to calculate minimum cost to move chips.

--- day_3/Solution1217.java
+++ day_3/Solution1217.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+       int even=0;
+       int odd=0;
+       
+       for(int i:position)
+       {
+        if(i%2==0)
+        even++;
+        else
+        odd++;
+       } 
+       return (even<odd)?even:odd;
+    }
+}
