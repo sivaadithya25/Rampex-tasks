2d3bce277684c10f71fbf4cadbcfe2b9f18d5206
Siva Adithya Narayana A <sivaadinarayanan25@gmail.com>
2026-03-24 09:21:08 +0530

    Implement bitwise operations in BitwiseOperator class

---
+++ day_/bitwise_operator.java
@@ -0,0 +1,13 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a = 5;  
+        int b = 3;  
+        System.out.println("Bitwise AND (a & b): " + (a & b)); 
+        System.out.println("Bitwise OR (a | b): " + (a | b));  
+        System.out.println("Bitwise XOR (a ^ b): " + (a ^ b)); 
+        System.out.println("Bitwise NOT (~a): " + (~a)); 
+        System.out.println("Left Shift (a << 1): " + (a << 1)); 
+        System.out.println("Right Shift (a >> 1): " + (a >> 1)); 
+    }
+    
+}
