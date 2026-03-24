ad5f3de1e1be269aefb496a5e79e62f233b4ea82
Siva Adithya Narayana A <sivaadinarayanan25@gmail.com>
2026-03-24 09:16:00 +0530

    java_concepts

---
+++ day_1/PrimitiveDT..java
@@ -0,0 +1,25 @@
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        // Primitive data types in Java
+        byte myByte = 100; // 8-bit signed integer
+        short myShort = 10000; // 16-bit signed integer
+        int myInt = 100000; // 32-bit signed integer
+        long myLong = 10000000000L; // 64-bit signed integer
+        float myFloat = 3.14f; // 32-bit floating-point number
+        double myDouble = 3.141592653589793; // 64-bit floating-point number
+        char myChar = 'A'; // 16-bit Unicode character
+        boolean myBoolean = true; // true or false
+
+        // Print the values of the primitive data types
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Int value: " + myInt);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Char value: " + myChar);
+        System.out.println("Boolean value: " + myBoolean);
+    }
+    
+}
