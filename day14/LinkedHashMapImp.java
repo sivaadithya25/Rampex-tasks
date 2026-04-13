1312e55dcdfbd0616154e5a1579428119c55cb2d
Siva Adithya Narayana A <sivaadinarayanan25@gmail.com>
2026-04-13 18:35:30 +0530

    concepts

---
+++ day14/LinkedHashMapImp.java
@@ -0,0 +1,13 @@
+package Day_14.Concepts_Code;
+
+import java.util.*;
+public class LinkedHashMapImplementation {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer,String> map=new LinkedHashMap<>();
+        map.put(3, "Three");
+        map.put(1, "One");
+        map.put(2,"Two");
+        System.out.println(map);
+    }
+    
+}
